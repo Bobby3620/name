95171e09c69396857c72c924880df7d819bc68a3
rd568 <bobbyday3620@gmail.com>
2018-11-09 11:19:58 -0500

    ayyyyyy

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,25 +2,25 @@ package com.company;
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
-import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException{
-	    Scanner knInput = new Scanner(new File("scores.txt"));
+    public static void main(String[] args) throws IOException {
+        Scanner kbInput = new Scanner(new File("scores.txt"));
         int maxIndex = -1;
         String text[] = new String[1000];
-        Double reading[] = new Double[1000];
-        Double writing[] = new Double[1000];
 
-        while(knInput.hasNext()){
+        //This prints out every line in the scores file
+        while(kbInput.hasNext()){
             maxIndex++;
-            //text[maxIndex]= knInput.useDelimiter("\\s*Percent White\\s*");
-            System.out.println();
-
+            text[maxIndex] = kbInput.nextLine();
         }
-        for(int i = 0; i < 1; i++)
-            System.out.println(text[i]);
+
+        kbInput.close();
+
+        //Prints out the schools
+
+
 
     }
 }
